0aac16d4e0dca62bb8f55f7cceb07a13493102f6
Simon Luijk <simon@simonluijk.com>
2017-03-05 10:14:10 +0100

    Separate variables and outputs

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-variable "account_id" {}
-variable "vpc_id" {}
-variable "vpc_cidr_block" {}
-
-# Roles that will be given access to EFS filesystem
-variable "roles" {}
-
-# Subnets where EFS mount targets will be created
-variable "subnets" {}
-variable "subnets_count" {}
-variable "filesystem_name" {}
 
 resource "aws_iam_policy" "efs_policy" {
   name = "EFS-${var.filesystem_name}-Access-Policy"
@@ -80,8 +69,4 @@ resource "aws_efs_mount_target" "shared_ecs_mount" {
     file_system_id = "${aws_efs_file_system.shared_ecs.id}"
     subnet_id = "${element(split(",", var.subnets), count.index)}"
     security_groups = ["${aws_security_group.efs.id}"]
-}
-
-output "security_group" {
-    value = "${aws_security_group.efs.id}"
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "account_id" {}
+variable "vpc_id" {}
+variable "vpc_cidr_block" {}
+
+# Roles that will be given access to EFS filesystem
+variable "roles" {}
+
+# Subnets where EFS mount targets will be created
+variable "subnets" {}
+variable "subnets_count" {}
+variable "filesystem_name" {}
